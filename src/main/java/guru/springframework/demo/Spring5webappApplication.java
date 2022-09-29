cba0e1b6947366173e0b3f28cc6b1f73264fcd8d
Wulfespirit <jasonkas2000@gmail.com>
2022-09-28 21:16:19 -0600

    More progress

--- src/main/java/guru/springframework/demo/Spring5webappApplication.java
+++ src/main/java/guru/springframework/demo/Spring5webappApplication.java
@@ -8,7 +8,7 @@ public class Spring5webappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
-		System.out.println("Is this printing?");
+		System.out.println("Test change");
 	}
 
 }
